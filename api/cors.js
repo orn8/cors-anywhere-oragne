5ad0d0ffddd0e73a2540cc5b5a1dc879c62515d3
oragne <me@oragne.dev>
2024-06-26 17:57:37 +1000

    Update cors.js

--- api/cors.js
+++ api/cors.js
@@ -7,16 +7,12 @@ export default async function handler(request, response) {
 
   response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
-
-  if (status === 200) {
-    if (contentType && contentType.startsWith('application/octet-stream')) {
-      response.setHeader('Content-Type', contentType);
-      response.setHeader('Content-Disposition', 'attachment');
-    }
-    response.status(status).send(data);
-  } else {
-    response.status(status).send({ error: 'Failed to fetch data' });
+  
+  if (contentType && contentType.startsWith('text/html')) {
+    data = stripHtmlTags(data);
   }
+
+  response.status(status).send(data);
 
   function getRequest(url) {
     return new Promise(resolve => {
@@ -24,35 +20,23 @@ export default async function handler(request, response) {
         let data = '';
         let contentType = resp.headers['content-type'];
 
-        if (resp.statusCode >= 300 && resp.statusCode < 400 && resp.headers.location) {
-          return resolve(getRequest(resp.headers.location));
-        }
-
-        if (contentType && contentType.startsWith('application/octet-stream')) {
-          const chunks = [];
-          resp.on('data', (chunk) => {
-            chunks.push(chunk);
-          });
-          resp.on('end', () => {
-            data = Buffer.concat(chunks);
-            resolve({ status: resp.statusCode, data: data, contentType: contentType });
-          });
-        } else {
-          resp.setEncoding('utf8');
-          resp.on('data', (chunk) => {
-            data += chunk;
-          });
-          resp.on('end', () => {
-            resolve({ status: resp.statusCode, data: data, contentType: contentType });
-          });
-        }
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+        resp.on('end', () => {
+          resolve({ status: resp.statusCode, data: data, contentType: contentType });
+        });
       });
 
       req.on('error', (err) => {
-        resolve({ status: 500, data: err.message });
+        resolve({ status: 500, data: err.message, contentType: null });
       });
 
       req.end();
-    });
+    }); 
+  }
+
+  function stripHtmlTags(html) {
+    return html.replace(/<\/?([a-z][a-z0-9]*)\b[^>]*>?/gi, '');
   }
 }
