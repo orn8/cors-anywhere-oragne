5b027c82b099a270ec2ce65c028481d743b7cbf7
oragne <me@oragne.dev>
2024-06-26 17:27:15 +1000

    redirection and file download support

--- api/cors.js
+++ api/cors.js
@@ -1,16 +1,10 @@
 export default async function handler(request, response) {
   const https = require('https');
 
-  let query = Object.entries(request.query);
-  query.shift();
   let url = request.query.url;
-  
-  query.forEach(entry => {
-    url += '&' + entry[0] + '=' + entry[1];
-  });
-  
+
   const { status, data } = await getRequest(url);
-  
+
   response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
   response.status(status).send(data);
@@ -19,6 +13,11 @@ export default async function handler(request, response) {
     return new Promise(resolve => {
       const req = https.get(url, (resp) => {
         let data = '';
+
+        if (resp.statusCode >= 300 && resp.statusCode < 400 && resp.headers.location) {
+          return resolve(getRequest(resp.headers.location));
+        }
+
         resp.on('data', (chunk) => {
           data += chunk;
         });
@@ -26,6 +25,12 @@ export default async function handler(request, response) {
           resolve({ status: resp.statusCode, data: data });
         });
       });
+
+      req.on('error', (err) => {
+        resolve({ status: 500, data: err.message });
+      });
+
+      req.end();
     }); 
   }
 }
