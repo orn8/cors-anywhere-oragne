f9e916985b1eeff9f12148177697ac3af01f138a
oragne <me@oragne.dev>
2024-06-26 17:47:10 +1000

    directly return data received instead of html when requesting file

--- api/cors.js
+++ api/cors.js
@@ -7,7 +7,12 @@ export default async function handler(request, response) {
 
   response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
-  response.status(status).send(data);
+  
+  if (status === 200) {
+    response.status(status).send(data);
+  } else {
+    response.status(status).send({ error: 'Failed to fetch data' });
+  }
 
   function getRequest(url) {
     return new Promise(resolve => {
@@ -18,12 +23,23 @@ export default async function handler(request, response) {
           return resolve(getRequest(resp.headers.location));
         }
 
-        resp.on('data', (chunk) => {
-          data += chunk;
-        });
-        resp.on('end', () => {
-          resolve({ status: resp.statusCode, data: data });
-        });
+        const contentType = resp.headers['content-type'];
+        if (!contentType || !contentType.startsWith('application/octet-stream')) {
+          let errorData = '';
+          resp.on('data', (chunk) => {
+            errorData += chunk;
+          });
+          resp.on('end', () => {
+            resolve({ status: resp.statusCode, data: errorData });
+          });
+        } else {
+          resp.on('data', (chunk) => {
+            data += chunk;
+          });
+          resp.on('end', () => {
+            resolve({ status: resp.statusCode, data: data });
+          });
+        }
       });
 
       req.on('error', (err) => {
