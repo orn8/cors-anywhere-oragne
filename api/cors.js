6bf91232f7b42cacf10679e555590069a30f6bee
oragne <me@oragne.dev>
2024-06-26 17:35:22 +1000

    improved error handling, fix file downloads

--- api/cors.js
+++ api/cors.js
@@ -3,31 +3,36 @@ export default async function handler(request, response) {
 
   let url = request.query.url;
 
-  const { status, data } = await getRequest(url);
+  const { status, data, contentType } = await getRequest(url);
 
   response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
-  response.status(status).send(data);
+  response.setHeader('Content-Type', contentType);
+  response.status(status);
+
+  if (contentType && contentType.startsWith('application')) {
+    response.setHeader('Content-Disposition', 'attachment');
+  }
+
+  response.send(data);
 
   function getRequest(url) {
     return new Promise(resolve => {
       const req = https.get(url, (resp) => {
-        let data = '';
-
-        if (resp.statusCode >= 300 && resp.statusCode < 400 && resp.headers.location) {
-          return resolve(getRequest(resp.headers.location));
-        }
+        const contentType = resp.headers['content-type'];
+        let data = Buffer.alloc(0);
 
         resp.on('data', (chunk) => {
-          data += chunk;
+          data = Buffer.concat([data, chunk]);
         });
+
         resp.on('end', () => {
-          resolve({ status: resp.statusCode, data: data });
+          resolve({ status: resp.statusCode, data: data, contentType: contentType });
         });
       });
 
       req.on('error', (err) => {
-        resolve({ status: 500, data: err.message });
+        resolve({ status: 500, data: err.message, contentType: 'text/plain' });
       });
 
       req.end();
