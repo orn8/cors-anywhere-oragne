d86e335c373c255aac96d704467927c5b36eebae
oragne <me@oragne.dev>
2024-10-26 13:38:32 +1100

    security update

--- api/cors.js
+++ api/cors.js
@@ -1,18 +1,29 @@
 export default async function handler(request, response) {
   const https = require('https');
 
+  const allowedOrigins = [
+    'https://kahootfetcher.oragne.dev'
+  ];
+
+  const origin = request.headers.origin;
+
+  if (!allowedOrigins.includes(origin)) {
+    return response.status(403).send('Forbidden: Access is denied.');
+  }
+
+  response.setHeader('Access-Control-Allow-Origin', origin);
+  response.setHeader('Access-Control-Allow-Headers', '*');
+
   let query = Object.entries(request.query);
   query.shift();
   let url = request.query.url;
-  
+
   query.forEach(entry => {
     url += '&' + entry[0] + '=' + entry[1];
   });
-  
+
   const { status, data } = await getRequest(url);
   
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Allow-Headers', '*');
   response.status(status).send(data);
 
   function getRequest(url) {
@@ -26,6 +37,10 @@ export default async function handler(request, response) {
           resolve({ status: resp.statusCode, data: data });
         });
       });
-    }); 
+
+      req.on('error', (err) => {
+        resolve({ status: 500, data: err.message });
+      });
+    });
   }
 }
