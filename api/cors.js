891c1d79f3dd5a848a6cdc90511e9681ae0f3c8f
oragne <me@oragne.dev>
2025-03-29 11:17:40 +1100

    initial commit

---
+++ api/cors.js
@@ -0,0 +1,47 @@
+export default async function handler(request, response) {
+  const https = require('https');
+
+  const allowedOrigins = [
+    'https://kahootfetcher.oragne.dev',
+    'https://vanishgames.oragne.dev'
+  ];
+
+  const origin = request.headers.origin;
+
+  if (!allowedOrigins.includes(origin)) {
+    return response.status(403).send('Forbidden: Access is denied.');
+  }
+
+  response.setHeader('Access-Control-Allow-Origin', origin);
+  response.setHeader('Access-Control-Allow-Headers', '*');
+
+  let query = Object.entries(request.query);
+  query.shift();
+  let url = request.query.url;
+
+  query.forEach(entry => {
+    url += '&' + entry[0] + '=' + entry[1];
+  });
+
+  const { status, data } = await getRequest(url);
+  
+  response.status(status).send(data);
+
+  function getRequest(url) {
+    return new Promise(resolve => {
+      const req = https.get(url, (resp) => {
+        let data = '';
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+        resp.on('end', () => {
+          resolve({ status: resp.statusCode, data: data });
+        });
+      });
+
+      req.on('error', (err) => {
+        resolve({ status: 500, data: err.message });
+      });
+    });
+  }
+}
